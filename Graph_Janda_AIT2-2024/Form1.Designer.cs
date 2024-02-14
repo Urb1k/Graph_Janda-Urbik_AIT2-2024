1aeabe9eb659865e17c31a2a0bbf88fa17e6fa14
Urb1k <vojtisek.urbanec@gmail.com>
2024-02-14 15:42:25 +0100

    Add project files.

---
+++ Graph_Janda_AIT2-2024/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Graph_Janda_AIT2_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.yearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.monthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datovyBodBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.datovyBodBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datovyBodBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datovyBodBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.addToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(794, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.addToolStripMenuItem.Text = "Add";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(543, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Export PNG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(543, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Export CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(24, 59);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(325, 330);
+            this.cartesianChart1.TabIndex = 5;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.yearDataGridViewTextBoxColumn,
+            this.monthDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.datovyBodBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(383, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(382, 142);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged_1);
+            // 
+            // yearDataGridViewTextBoxColumn
+            // 
+            this.yearDataGridViewTextBoxColumn.DataPropertyName = "year";
+            this.yearDataGridViewTextBoxColumn.HeaderText = "year";
+            this.yearDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.yearDataGridViewTextBoxColumn.Name = "yearDataGridViewTextBoxColumn";
+            this.yearDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // monthDataGridViewTextBoxColumn
+            // 
+            this.monthDataGridViewTextBoxColumn.DataPropertyName = "month";
+            this.monthDataGridViewTextBoxColumn.HeaderText = "month";
+            this.monthDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.monthDataGridViewTextBoxColumn.Name = "monthDataGridViewTextBoxColumn";
+            this.monthDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "value";
+            this.valueDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            this.valueDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // datovyBodBindingSource1
+            // 
+            this.datovyBodBindingSource1.DataSource = typeof(Graph_Janda_AIT2_2024.DatovyBod);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(543, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Import";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // datovyBodBindingSource
+            // 
+            this.datovyBodBindingSource.DataSource = typeof(Graph_Janda_AIT2_2024.DatovyBod);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(543, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Print";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 440);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datovyBodBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datovyBodBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn monthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource datovyBodBindingSource;
+        private System.Windows.Forms.BindingSource datovyBodBindingSource1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Graph_Janda_AIT2-2024/Form1.cs
@@ -0,0 +1,263 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace Graph_Janda_AIT2_2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            UpdateGraph();
+        }
+        public void UpdateGraph()
+        {
+            if (datovyBodBindingSource1.DataSource == null)
+            {
+                return;
+            }
+
+            cartesianChart1.Series.Clear();
+            SeriesCollection series = new SeriesCollection();
+            var years = (from o in datovyBodBindingSource1.DataSource as List<DatovyBod> select new { Year = o.year }).Distinct();
+
+            foreach (var year in years)
+            {
+                List<double> values = new List<double>();
+                for (int month = 1; month <= 12; month++)
+                {
+                    double value = 0;
+                    var data = from o in datovyBodBindingSource1.DataSource as List<DatovyBod>
+                               where o.year.Equals(year.Year) && o.month.Equals(month)
+                               orderby o.month ascending
+                               select new { o.value, o.month };
+
+                    if (data.SingleOrDefault() != null)
+                    {
+                        value = data.SingleOrDefault().value;
+                        values.Add(value);
+                    }
+                }
+
+                series.Add(new LineSeries()
+                {
+                    Title = year.Year.ToString(),
+                    Values = new ChartValues<double>(values)
+
+                });
+
+            }
+            cartesianChart1.Series = series;
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            datovyBodBindingSource1.DataSource = new List<DatovyBod>();
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Title = "Months",
+                Labels = new[] { "leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec" }
+            }); ;
+
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "Value",
+            });
+
+            cartesianChart1.LegendLocation = LegendLocation.Right;
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellValueChanged_1(object sender, DataGridViewCellEventArgs e)
+        {
+            UpdateGraph();
+        }
+        private void ExportDataToCSV()
+        {
+            var data = (List<DatovyBod>)datovyBodBindingSource1.DataSource;
+            if (data == null || data.Count == 0)
+            {
+                MessageBox.Show("No data to export.");
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                Title = "Export to CSV",
+
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.WriteLine("Year,Month,Value");
+                    foreach (var item in data)
+                    {
+                        sw.WriteLine($"{item.year},{item.month},{item.value}");
+                    }
+                }
+
+                MessageBox.Show($"Data exported successfully to {filePath}", "Export CSV", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExportDataToCSV();
+        }
+        private void ExportChartToPNG()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PNG files (*.png)|*.png",
+                Title = "Export Chart",
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                string filePath = saveFileDialog.FileName;
+                Bitmap chartImage = new Bitmap(cartesianChart1.Width, cartesianChart1.Height);
+                cartesianChart1.DrawToBitmap(chartImage, new Rectangle(0, 0, chartImage.Width, chartImage.Height));
+
+                chartImage.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+
+                MessageBox.Show($"Chart exported successfully to {filePath}", "Export PNG", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExportChartToPNG();
+        }
+
+        public void ImportFromCVS()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                Title = "Import CSV",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                List<DatovyBod> importedData = new List<DatovyBod>();
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    sr.ReadLine();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] values = sr.ReadLine().Split(',');
+                        if (values.Length == 3 && int.TryParse(values[0], out int year) && int.TryParse(values[1], out int month) && int.TryParse(values[2], out int dataValue))
+                        {
+                            importedData.Add(new DatovyBod { year = year, month = month, value = dataValue });
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid data format in CSV file.", "Import CSV", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                }
+                datovyBodBindingSource1.DataSource = importedData;
+                UpdateGraph();
+
+                MessageBox.Show($"Data imported successfully", "Import CSV", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ImportFromCVS();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (PrintDocument pd = new PrintDocument())
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pd.PrinterSettings.PrinterName = @"\\192.168.193.63\HP_LaserJet_M1120n";
+                    pd.PrintPage += (s, ev) => pd_PrintPage(s, ev, openFileDialog.FileName);
+                    pd.Print();
+                }
+            }
+        }
+
+        private void pd_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e, string filePath)
+        {
+
+            if (System.IO.File.Exists(filePath))
+            {
+                try
+                {
+                    using (System.Drawing.Image img = System.Drawing.Image.FromFile(filePath))
+                    {
+                        float aspectRatio = img.Width / (float)img.Height;
+
+                        float printableWidth = e.MarginBounds.Width;
+                        float printableHeight = printableWidth / aspectRatio;
+
+                        if (printableHeight > e.MarginBounds.Height)
+                        {
+                            printableHeight = e.MarginBounds.Height;
+                            printableWidth = printableHeight * aspectRatio;
+                        }
+
+                        float xPos = (e.MarginBounds.Width - printableWidth) / 2 + e.MarginBounds.Left;
+                        float yPos = (e.MarginBounds.Height - printableHeight) / 2 + e.MarginBounds.Top;
+
+                        e.Graphics.DrawImage(img, xPos, yPos, printableWidth, printableHeight);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while loading the image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                MessageBox.Show($"The document {filePath} has been succesfully sent to the printer. Wait for the printer.");
+            }
+            else
+            {
+                MessageBox.Show("The selected file is invalid or empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
